514281929d589ec25db0c0f127cc5c367911c2d0
Marcos Alarcon <86171565+alarconmarcos@users.noreply.github.com>
2022-08-24 15:23:24 -0300

    Add files via upload

---
+++ Tetris2.pas
@@ -0,0 +1,38 @@
+unit Tetris2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, Buttons, ExtCtrls;
+
+type
+  TTetro2 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    BitBtn1: TBitBtn;
+    Bevel1: TBevel;
+    Label9: TLabel;
+    Bevel2: TBevel;
+    Image1: TImage;
+    Bevel3: TBevel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Tetro2: TTetro2;
+
+implementation
+
+{$R *.DFM}
+
+end.
